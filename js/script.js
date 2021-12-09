d3ffa528dac70a000f4ab3b83aa0f2bf549c0a1d
TimeJumper1 <benassai.stefano.1@gmail.com>
2021-12-09 15:56:36 +0100

    milestone 1 completata

--- js/script.js
+++ js/script.js
@@ -5,10 +5,28 @@ const app = new Vue(
     {
         el: '#root',
         data: {
+            todo: [
+                {
+                    text:'completare la prima milestone',
+                    done: true
+                },
+                {
+                    text:'completare la seconda  milestone',
+                    done: false
+                },
+                {
+                    text:'completare la terza milestone',
+                    done: false
+                },
 
-        },
-        methods: {
+            ],
+            newTodo: '',
+
             
+        },
+        
+        methods: {
+
         }
     }
 );
